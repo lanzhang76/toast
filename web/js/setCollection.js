434cde26a1871b90c1182be14f7b91ce102fb6e8
Anna <annagarbier@gmail.com>
2019-12-09 18:21:35 -0500

    Ask the user to confirm when changing a collection

--- web/js/setCollection.js
+++ web/js/setCollection.js
@@ -2,6 +2,12 @@ var radios = document.getElementsByName("collection");
 
 for(radio in radios) {
     radios[radio].onclick = function() {
-        currentWordbank = this.value;
+    	var prettyCollectionName = this.value.replace(/_/g,  ' ').replace('.js', '').toUpperCase();
+    	var message = `By changing the collection, you are about to start a new session. Continue to ${prettyCollectionName} collection?`
+    	if (window.confirm(message)) {
+    		currentWordbank = this.value;
+		} else {
+			return false;
+		}
     }
 }
