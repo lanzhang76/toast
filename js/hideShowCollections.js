ccf3587dccfcd1630ccd2c50e1eb7ffc2e92db05
Lan Zhang <zhany016@newschool.edu>
2019-12-10 18:51:52 -0500

    update

--- js/hideShowCollections.js
+++ js/hideShowCollections.js
@@ -1,13 +1,17 @@
 var buttonText = document.getElementById("collections_button");
 
-buttonText.addEventListener('click', function (event) {
-	var collectionInfo = document.getElementById("toggleable_elements");
+buttonText.addEventListener(
+  "click",
+  function(event) {
+    var collectionInfo = document.getElementById("toggleable_elements");
 
-	if (collectionInfo.style.display === "none") {
-		collectionInfo.style.display = "block";
-		buttonText.innerHTML = 'Hide Collections'; 
-	} else {
-		collectionInfo.style.display = "none";
-		buttonText.innerHTML = 'Show Collections'; 
-	}
-}, false);
+    if (collectionInfo.style.display === "none") {
+      collectionInfo.style.display = "block";
+      buttonText.innerHTML = "Hide Collections";
+    } else {
+      collectionInfo.style.display = "none";
+      buttonText.innerHTML = "Show Collections";
+    }
+  },
+  false
+);
