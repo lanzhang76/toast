cec9464a220274852151ecf1436281a594e1183f
Lan Zhang <zhany016@newschool.edu>
2019-12-12 15:19:59 -0500

    add seinfeld

--- js/setCollection.js
+++ js/setCollection.js
@@ -1,22 +1,24 @@
 var radios = document.getElementsByName("collection");
 
-for(radio in radios) {
-    radios[radio].onclick = function() {
-    	var prettyCollectionName = this.value.replace(/_/g,  ' ').toUpperCase();
-    	var message = `By changing the collection, you are about to start a new session. Continue to ${prettyCollectionName} collection?`
-    	if (window.confirm(message)) {
-			drawAllHint("hide<br>hints");
-      		hintButton.innerHTML = "show<br>hints";
-			if (this.value === "animals") {
-				currentWordbank = animals;
-			} else if (this.value === "alice") {
-				currentWordbank = alice;
-			} else {
-				currentWordbank = bag_of_nouns;
-			}
-			resetAppData();
-		} else {
-			return false;
-		}
+for (radio in radios) {
+  radios[radio].onclick = function() {
+    var prettyCollectionName = this.value.replace(/_/g, " ").toUpperCase();
+    var message = `By changing the collection, you are about to start a new session. Continue to ${prettyCollectionName} collection?`;
+    if (window.confirm(message)) {
+      drawAllHint("hide<br>hints");
+      hintButton.innerHTML = "show<br>hints";
+      if (this.value === "animals") {
+        currentWordbank = animals;
+      } else if (this.value === "alice") {
+        currentWordbank = alice;
+      } else if (this.value === "seinfeld") {
+        currentWordbank = seinfeld;
+      } else {
+        currentWordbank = bag_of_nouns;
+      }
+      resetAppData();
+    } else {
+      return false;
     }
+  };
 }
